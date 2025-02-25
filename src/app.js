5dde107be864e60369d6e14fb5a297bb9d937cb0
jihyeon-Do <ehwlgus22@gmail.com>
2025-02-26 05:15:13 +0900

    docs : Edit comments

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-// 라우터 연결
+//: 라우터 연결
 app.use('/api', userRoutes);
 
-// 서버 실행
+//: 서버 실행
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
